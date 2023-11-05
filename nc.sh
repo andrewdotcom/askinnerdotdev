e3a03d30e60857cfd318e873bdd6f53d9e4eaa19
Andrew Skinner <ajskinner@gmail.com>
2023-11-05 18:58:15 +1100

    added draft post

--- nc.sh
+++ nc.sh
@@ -10,5 +10,5 @@ else
     title=$1
     file="$(tr '[:upper:]' '[:lower:]' <<< "$1")"
     description=$2
-    echo $'+++\ntitle = "'${title}$'"\ndescription = "'${description}$'"\ntags = ["'${tags}$'"]\ndraft=true\n+++' > ./content/"${now}-${file// /-}.md"
+    echo $'+++\ntitle = "'${title}$'"\ndescription = "'${description}$'"\ntags = ["'${tags}$'"]\ndate = '${now}$'\ndraft=true\n+++' > ./content/blog/"${now}-${file// /-}.md"
 fi
